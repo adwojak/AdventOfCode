45eef12c5e466c798dda7dd36856054f6a24592c
Artur Dwojak <dwojaka207@gmail.com>
2020-12-01 22:46:04 +0100

    Day 1

---
+++ Day1/main.py
@@ -0,0 +1,22 @@
+from functools import reduce
+from itertools import combinations
+
+SUM_VALUE = 2020
+
+with open('input') as f:
+    input_data = [int(val) for val in f.read().split('\n')]
+
+
+def get_multiplication(sum_value: int, numbers_of_sum: int):
+    try:
+        combination = [comb for comb in combinations(input_data, numbers_of_sum) if sum(comb) == sum_value][0]
+        return reduce((lambda x, y: x * y), combination)
+    except IndexError:
+        return 0
+
+
+# FIRST STAR
+print(get_multiplication(SUM_VALUE, 2))
+
+# SECOND STAR
+print(get_multiplication(SUM_VALUE, 3))
---
+++ Day1/tests.py
@@ -0,0 +1,24 @@
+from pytest import raises
+from Day1.main import get_multiplication
+
+
+class TestDay1:
+    def test_two_numbers(self):
+        assert get_multiplication(2020, 2) == 365619
+
+    def test_three_numbers(self):
+        assert get_multiplication(2020, 3) == 236873508
+
+    def test_no_numbers(self):
+        assert get_multiplication(2020, 0) == 0
+
+    def test_sum_value_zero(self):
+        assert get_multiplication(0, 2) == 0
+
+    def test_bad_values_provided(self):
+        with raises(TypeError):
+            get_multiplication('a', 'b')
+
+    def test_missing_arguments(self):
+        with raises(TypeError):
+            get_multiplication()
